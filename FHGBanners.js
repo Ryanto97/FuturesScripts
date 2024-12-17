0bf592723a07ab3fe7e78ebd81205542142da3d6
Ryanto97 <ryan.toft@futureshg.co.uk>
2024-12-17 15:38:27 +0000

    Added in addtional function to hide banner

--- FHGBanners.js
+++ FHGBanners.js
@@ -2,7 +2,7 @@ function setcontext_onload(executionContext) {
 	var formcontext = executionContext.getFormContext();
 
 	AlertIfMedicalIsYes(formcontext);
-    deceasedCustomer(formcontext);
+       deceasedCustomer(formcontext);
 }
 
 
@@ -10,10 +10,10 @@ function AlertIfMedicalIsYes(formcontext) {
 
 	var isMedical = formcontext.getAttribute("fhg_medicalneed").getValue();
 	var medicalNeed =  formcontext.getAttribute("fhg_medicalneed").getText();
-	var message = "There is a medical need on the account: " + medicalNeed + ". Please see Communication Preferences and Medical Needs"; 
+	var message = "There is a medical need on the account: " + medicalNeed + " please see Communication Preferences and Medical Needs"; 
 	
 
-	if (isMedical) { 
+	if (isMedical !== 100000006 &&(isMedical !== null)) { //no medical need//not null 
 	//pop up alert
 	alert("WARNING: This Contact has a medical on it");
 	//banner
@@ -25,7 +25,7 @@ function AlertIfMedicalIsYes(formcontext) {
 function deceasedCustomer(formcontext) { 
 
 	var deceasedCustomer = formcontext.getAttribute("fhg_datedeceased").getValue();
-	var message = "NOTE: Customer is Deceased"; 
+	var message = "NOTE: Customer Is deceased"; 
 	
 
 	if (deceasedCustomer === null ) { 
